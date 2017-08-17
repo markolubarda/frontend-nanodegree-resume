b19366ab89ff738a8e4ee7177b2006bb8acd3151
marko lubarda <marko@ballab.com>
2017-08-17 17:19:34 +0200

    first commit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+$("#main").append("Marko Lubarda");
+
+var firstName = "marko";
+
+var age = 32;
+
+console.log(firstName, age);
